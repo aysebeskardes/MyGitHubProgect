0c2e8993d005a577f62dbcd343309e473193dcc8
ayse <sabriyebeskardes@gmail.com>
2023-01-31 14:41:07 -0500

    second commit no changes

--- src/Day1GitHub/SecondGitHubClass.java
+++ src/Day1GitHub/SecondGitHubClass.java
@@ -3,6 +3,6 @@ package Day1GitHub;
 public class SecondGitHubClass {
     public static void main(String[] args) {
         System.out.println("Hello batch 7");
-        System.out.println(1+1);
+
     }
 }
